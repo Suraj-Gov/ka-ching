3a98bbd8e1c68eb9bc1e8ee1482b942a5703f5a8
Suraj Govind <surajgovind332000@gmail.com>
2023-03-12 18:56:09 +0530

    random align

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useRef } from "react";
 import Emoji from "./components/Emoji";
 
 const SlotRoll = forwardRef<HTMLDivElement, { n: number }>(({ n }, ref) => {
   return (
     <div ref={ref} className="flex gap-8 flex-col items-center">
-      {[...Array(n)].map((_) => (
-        <Emoji />
+      {[...Array(n)].map((_, idx) => (
+        <Emoji key={idx} />
       ))}
     </div>
   );
 });
 
+// considering 10 items in a SlotRoll
+const N = 10;
 function App() {
+  const roll1Ref = useRef<HTMLDivElement>(null);
+  const roll2Ref = useRef<HTMLDivElement>(null);
+  const roll3Ref = useRef<HTMLDivElement>(null);
+
+  const initPositionRolls = () => {
+    const h = roll1Ref.current!.getBoundingClientRect().height;
+    const itemH = roll1Ref.current!.children[0].getBoundingClientRect().height;
+    const gap = (h - itemH * 10) / (N - 1);
+
+    const rolls = [roll1Ref, roll2Ref, roll3Ref];
+    rolls.forEach((r) => {
+      const rand = ~~(Math.random() * (N - 3));
+      const offset = rand * (itemH + gap);
+      r.current?.animate(
+        [
+          { transform: "translateY(0px)" },
+          { transform: `translateY(${-offset + gap / 2}px)` },
+        ],
+        { fill: "forwards", duration: 100 * rand, easing: "ease" }
+      );
+    });
+  };
+
   return (
-    <div className="bg-bg h-full">
-      <div className="flex justify-center items-center h-full">
-        <div
-          style={{ aspectRatio: "3 / 2" }}
-          className="max-w-xs w-full border-gray-50 border-2 grid grid-cols-3 place-items-center overflow-hidden"
-        >
-          <SlotRoll n={5} />
-          <SlotRoll n={5} />
-          <SlotRoll n={5} />
+    <div className="bg-bg h-full text-textMain">
+      <div className="flex flex-col justify-center items-center h-full">
+        <div className="relative h-[14rem] w-[18rem] border-[#804545] border-2 grid grid-cols-3 overflow-hidden">
+          <SlotRoll ref={roll1Ref} n={N} />
+          <div className="slit w-[2px] h-[14rem] absolute left-1/3 top-1/2 -translate-y-1/2"></div>
+          <SlotRoll ref={roll2Ref} n={N} />
+          <div className="slit w-[2px] h-[14rem] absolute right-1/3 top-1/2 -translate-y-1/2"></div>
+          <SlotRoll ref={roll3Ref} n={N} />
         </div>
+        <div className="h-10"></div>
+        <button onClick={initPositionRolls}>ka-ching</button>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        bg: "#1b0a0a"
-      }
+        bg: "#1b0a0a",
+        textMain: "#fab5bf",
+      },
     },
   },
   plugins: [],
-}
+};
